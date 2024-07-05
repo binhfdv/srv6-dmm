594a92f76bbd0d5f0e1dc33561e9d296168d3e99
binhfdv <dvanbinh10@gmail.com>
2024-07-05 17:05:27 +0900

    prometheus install

---
+++ manager_worker/prometheus/install-prometheus.sh
@@ -0,0 +1,4 @@
+
+sudo curl -LO https://github.com/prometheus/prometheus/releases/download/v2.51.1/prometheus-2.51.1.linux-amd64.tar.gz
+sudo tar -xzvf prometheus-2.51.1.linux-amd64.tar.gz
+sudo cp -f prometheus.yml prometheus-2.51.1.linux-amd64/
